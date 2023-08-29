68a60113c39a3ed5de70462f1c5ba48888816570
Ashish <ashish74624@gmail.com>
2023-08-29 23:01:27 +0530

    corection in routes

--- server/routes/docRoutes.js
+++ server/routes/docRoutes.js
@@ -1,7 +1,4 @@
 import express from 'express'
-import User from '../models/user.js';
-import Doc from '../models/doc.js';
-import Folder from '../models/folder.js';
 import { addFolder , deleteFolder } from '../controllers/docController.js'
 
 const router = express.Router();
