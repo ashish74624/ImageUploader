dc946a32cba70163e3a98546c28b03f6a514837f
Ashish <ashish74624@gmail.com>
2023-12-15 23:01:41 +0530

    First slide complete

--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -1,15 +1,22 @@
 'use client'
 import Carousel from "nuka-carousel"
+import { Link} from 'react-router-dom'
 
 
 export default function Slider() {
   return (
     <Carousel className='bg-blue-500 w-screen h-[90vh] rounded-lg py-[4vh]'>
-      <div className="h-[90vh] w-[95vw] rounded-lg mr-10 mx-8 p-8 flex bg-gray-100">
-        <div className="">
+      <div className="h-[90vh] w-[95vw] rounded-lg mx-8 flex bg-gray-100 overflow-hidden shadow-lg">
+        <div className="p-8">
           <h2 className="text-gray-900 text-8xl">Step 1:</h2>
-          <h4 className="text-gray-900 text-5xl mt-2">Create an Account</h4>
+          <h4 className="text-gray-900 text-5xl mt-4">Create an Account</h4>
           <p className="text-gray-900 text-xl mt-4">Add Information such as Name , Email and password and create an account</p>
+        </div>
+        <div className="relative group">
+          <Link to={'/register'} className=" bg-gray-800 absolute -bottom-10 group-hover:bottom-0 w-full h-32 flex justify-center items-center z-10 text-white bg-opacity-0 opacity-0 group-hover:opacity-100 transition-all duration-300 group-hover:bg-opacity-50">
+            Go To Register Page
+          </Link>
+          <img className=" h-full w-[750px]" src="/s1.png" alt="" />
         </div>
       </div>
       <div className="h-[90vh] w-full rounded-lg px-8">
